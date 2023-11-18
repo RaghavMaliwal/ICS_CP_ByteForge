5f7e4a0b848a47758336a608cd10281e80e7a925
RaghavMaliwal <141847608+RaghavMaliwal@users.noreply.github.com>
2023-11-18 21:27:07 +0530

    Add files via upload

---
+++ switchcase.c
@@ -0,0 +1,167 @@
+#include<stdio.h>
+                    //note-pre post multi
+int main(){
+
+    printf("\n                                   WELCOME TO THE MATRIX \n");
+    printf("                              A project made by ADITYA,KRISH,RAGHAV,SOHOM\n");
+
+    // printf("\nTo perform Matrix operation : ENTER 1\n");
+    // printf("\nTo know the type of your matrix : ENTER 2\n");
+    //printf("\nTo know the type of your matrix : ENTER 3\n");
+
+    printf("\nSum                 :1\nDifference          :2\nMultiplication      :3\nScalar Multipication:4\nTranspose           :5\nCofactor            :6\nDeterminant          :7\nAdjoint             :8\nInverse             :9\n");
+    int input_specifier;    
+    printf("\nENTER THE VALUE:");
+    scanf("%d",&input_specifier);
+
+    /*switch(input_specifier)
+
+        case 1 : 
+            printf(" Do you want sum_of_matrix\n IF YES:ENTER 'Y' ELSE 'N' ");
+            char confirmation_specifier;
+            scanf("%c",&confirmation_specifier);
+
+            if(confirmation_specifier == 'Y'){
+                sum_of_matrix();
+            break;
+            }
+
+            else{
+                input();
+            }
+            
+
+        case 2 : 
+            printf("Do you want difference_of_matrix\n IF YES:ENTER 'Y' ELSE 'N' ");
+            char confirmation_specifier;
+            scanf("%c",&confirmation_specifier);
+
+            if(confirmation_specifier == 'Y'){
+                difference_of_matrix();
+            break;
+            }
+
+            else{
+                input();
+            }
+            
+        
+        case 3 : 
+            printf("Do you want multiplication_of_matrix\n IF YES:ENTER 'Y' ELSE 'N' ");
+            char confirmation_specifier;
+            scanf("%c",&confirmation_specifier);
+
+            if(confirmation_specifier == 'Y'){
+                multiplication_of_matrix();
+            break;
+            }
+
+            else{
+                input();
+            }
+            
+        
+        case 4 : 
+        printf("Do you want scalar_multiplication_of_matrix\n IF YES:ENTER 'Y' ELSE 'N' ");
+            char confirmation_specifier;
+            scanf("%c",&confirmation_specifier);
+
+            if(confirmation_specifier == 'Y'){
+                scalar_multiplication_of_matrix();
+            break;
+            }
+
+            else{
+                input();
+            }
+            
+        
+        case 5 :
+        printf("Do you want transpose_of_matrix\n IF YES:ENTER 'Y' ELSE 'N' ");
+            char confirmation_specifier;
+            scanf("%c",&confirmation_specifier);
+
+            if(confirmation_specifier == 'Y'){
+                transpose_of_matrix();
+            break;
+            }
+
+            else{
+                input();
+            } 
+            
+
+        case 6 : 
+        printf("Do you want cofactor_of_matrix\n IF YES:ENTER 'Y' ELSE 'N' ");
+            char confirmation_specifier;
+            scanf("%c",&confirmation_specifier);
+
+            if(confirmation_specifier == 'Y'){
+                cofactor_of_matrix();
+            break;
+            }
+
+            else{
+                input();
+            }
+
+        case 7 : 
+        printf("Do you want determinant_of_matrix\n IF YES:ENTER 'Y' ELSE 'N' ");
+            char confirmation_specifier;
+            scanf("%c",&confirmation_specifier);
+
+            if(confirmation_specifier == 'Y'){
+                cofactor_of_matrix();
+            break;
+            }
+
+            else{
+                input();
+            }
+        
+        case 8 : 
+        printf("Do you want adjoint_of_matrix\n IF YES:ENTER 'Y' ELSE 'N' ");
+            char confirmation_specifier;
+            scanf("%c",&confirmation_specifier);
+
+            if(confirmation_specifier == 'Y'){
+                cofactor_of_matrix();
+            break;
+            }
+
+            else{
+                input();
+            }
+        
+        case 9 : 
+        printf("Do you want inverse_of_matrix\n IF YES:ENTER 'Y' ELSE 'N' ");
+            char confirmation_specifier;
+            scanf("%c",&confirmation_specifier);
+
+            if(confirmation_specifier == 'Y'){
+                cofactor_of_matrix();
+            break;
+            }
+
+            else{
+                input();
+            }*/
+
+/*       case 10 : 
+        printf("Do you want _of_matrix\n IF YES:ENTER 'Y' ELSE 'N' ")
+            char confirmation_specifier;
+            scanf("%c",&confirmation_specifier);
+
+            if(confirmation_specifier == 'Y'){
+                cofactor_of_matrix();
+            break;
+            }
+
+            else{
+                input();
+            } 
+              } 
+*/          
+    
+
+}
